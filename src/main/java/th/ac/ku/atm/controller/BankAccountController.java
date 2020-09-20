fd3c53d2012fda9fac6c9927b7482d99cb21c781
viva <vivabieber69@gmail.com>
2020-09-20 20:16:56 +0700

    update bankaccount html, class, service, controller

--- src/main/java/th/ac/ku/atm/controller/BankAccountController.java
+++ src/main/java/th/ac/ku/atm/controller/BankAccountController.java
@@ -1,13 +1,34 @@
 package th.ac.ku.atm.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import th.ac.ku.atm.model.BankAccount;
+import th.ac.ku.atm.service.BankAccountService;
 
 @Controller
+@RequestMapping("/bankaccount")
 public class BankAccountController {
 
-    @RequestMapping("/bankaccount")
-    public String getHomePage() {
+    private BankAccountService bankAccountService;
+
+    public BankAccountController(BankAccountService bankAccountService) {
+        this.bankAccountService = bankAccountService;
+    }
+
+    @GetMapping
+    public String getBankAccountPage(Model model) {
+        model.addAttribute("allAccounts", bankAccountService.getBankAccounts());
         return "bankaccount";
+    }
+
+    @PostMapping
+    public String registerBankAccount(@ModelAttribute BankAccount bankAccount, Model model) {
+        bankAccountService.createBankAccount(bankAccount);
+        model.addAttribute("allAccounts", bankAccountService.getBankAccounts());
+        return "redirect:bankaccount";
     }
 }
--- src/main/java/th/ac/ku/atm/service/BankAccountService.java
+++ src/main/java/th/ac/ku/atm/service/BankAccountService.java
@@ -1,4 +1,35 @@
 package th.ac.ku.atm.service;
 
+import org.springframework.stereotype.Service;
+import th.ac.ku.atm.model.BankAccount;
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class BankAccountService {
+
+    private ArrayList<BankAccount> bankAccountList;
+
+    @PostConstruct
+    public void postContruct() {
+        bankAccountList = new ArrayList<>();
+    }
+
+    public void createBankAccount(BankAccount bankAccount) {
+        // .... hash pin ....
+        bankAccountList.add(bankAccount);
+    }
+
+    public List<BankAccount> getBankAccounts() {
+        return new ArrayList<>(this.bankAccountList);
+    }
+
+    public BankAccount findBankAccount(int id) {
+        for (BankAccount bankAccount : bankAccountList) {
+            if (bankAccount.getId() == id)
+                return bankAccount;
+        }
+        return null;
+    }
 }
