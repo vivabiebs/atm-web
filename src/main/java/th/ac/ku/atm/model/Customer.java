fd3d15ae7f9e044dc94d3ec47dc36c3c5568fa43
viva <vivabieber69@gmail.com>
2020-09-27 20:33:11 +0700

    change connection to MYSQL from JDBC to JPA

--- src/main/java/th/ac/ku/atm/model/Customer.java
+++ src/main/java/th/ac/ku/atm/model/Customer.java
@@ -1,9 +1,18 @@
 package th.ac.ku.atm.model;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
 public class Customer {
+
+    @Id
     private int id;
+
     private String name;
     private String pin;
+
+    public Customer() {
+    }
 
     public Customer(int id, String name, String pin) {
         this.id = id;
@@ -33,5 +42,10 @@ public class Customer {
 
     public void setPin(String pin) {
         this.pin = pin;
+    }
+
+
+    public Customer get() {
+        return new Customer();
     }
 }
